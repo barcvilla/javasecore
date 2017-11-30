45a7c3f3c2bd7669c9a09f661ede5fe5ec0ce532
Carlos Villanueva <ceva_19@hotmail.com>
2017-11-30 11:29:11 -0300

    Java SE 8 Streams

--- corejava8vol2/src/core/ch01/stream/ex2/Test_Stream.java
+++ corejava8vol2/src/core/ch01/stream/ex2/Test_Stream.java
@@ -5,7 +5,14 @@
  */
 package core.ch01.stream.ex2;
 
+import core.ch01.stream.ex1.stream01;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,6 +22,20 @@ import java.util.stream.Stream;
  */
 public class Test_Stream 
 {
+    public static String readFile(String file)
+    {
+        String contents = "";
+        try
+        {
+            contents = new String(Files.readAllBytes(Paths.get("./rs/" + file + ".txt")), StandardCharsets.UTF_8);
+        }
+        catch(IOException ex)
+        {
+            Logger.getLogger(stream01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return contents;
+    }
+    
     public static <T> void iterarStream(Stream<T> stream)
     {
         List<T> elements = stream.collect(Collectors.toList());
@@ -32,8 +53,16 @@ public class Test_Stream
         iterarStream(randoms);
     }
     
+    public static void demoStreamSkip()
+    {
+        String fileSource = readFile("skip_text");
+        Stream<String> words = Stream.of(fileSource.split("\\PL+")).skip(1);
+        iterarStream(words);
+    }
+    
     public static void main(String[] args) 
     {
         demoExtractingSubStream();
+        demoStreamSkip();
     }
 }
