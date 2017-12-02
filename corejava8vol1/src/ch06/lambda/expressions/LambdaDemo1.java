a5024c2681fc6e9b89855064dd7194540c483a01
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-02 18:43:59 -0300

    Working with Lambda Functions

--- corejava8vol1/src/ch06/lambda/expressions/LambdaDemo1.java
+++ corejava8vol1/src/ch06/lambda/expressions/LambdaDemo1.java
@@ -10,21 +10,37 @@ package ch06.lambda.expressions;
  * @author barcvilla
  */
 public class LambdaDemo1 {
+    public void printSomething(String something, Printer printer)
+    {
+        printer.print(something);
+    }
     
     public static void usingLambda1()
     {
-        String header = "Simple use Lambda";
+        String header = "Simple use Lambda # 1}";
         ClassicSample.printHeader(header);
         
         LambdaDemo1 demo1 = new LambdaDemo1();
-        String something = "I am learning Lambda";
-        Printer printer = (String toPrint) -> {System.out.println(something);};
+        String message = "I am learning Lambda";
+        Printer printer = (String toPrint) -> {System.out.println(toPrint);};
+        demo1.printSomething(message, printer);
+        
         // otras formas mas consisas del uso de lambda
         // Printer printer = (toPrint) -> {System.out.println(somethign);};
         // Printer printer = toPrint -> System.out.println(something);
     }
     
+    public static void usingLambda2()
+    {
+        String header = "Simple use Lambda #2";
+        ClassicSample.printHeader(header);
+        LambdaDemo1 demo2 = new LambdaDemo1();
+        String message = "I am Lambda";
+        demo2.printSomething(message, (toPrint) -> {System.out.println(toPrint);});
+    }
+    
     public static void main(String[] args) {
         usingLambda1();
+        usingLambda2();
     }
 }
