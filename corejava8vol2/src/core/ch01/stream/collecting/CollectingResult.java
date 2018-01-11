5e50789e9fa016210bc00276a719b8ef0c6d5fdd
Carlos Villanueva <ceva_19@hotmail.com>
2018-01-11 12:05:13 -0300

    Working with Stream Colleccting

--- corejava8vol2/src/core/ch01/stream/collecting/CollectingResult.java
+++ corejava8vol2/src/core/ch01/stream/collecting/CollectingResult.java
@@ -10,6 +10,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,7 +19,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Cuando hacemos cosas con stream, a menudo queremos ver sus elemento.
  * @author barcvilla
  */
 public class CollectingResult 
@@ -42,13 +43,62 @@ public class CollectingResult
     public static <T> void show(String label, Set<T> set)
     {
         System.out.println(label + ": " + set.getClass().getName());
+        // Los objetos en el stream no son String, asi que los convertirmos a String
         System.out.println("[" + set.stream().map(Object::toString)
-        .collect(Collectors.joining(", ")) + "]");
+        // concatenamos los elementos de un stream mas un delimitador ", "
+        .collect(Collectors.joining(", ")) + "]"); 
+    }
+    
+    public static void printReadFileDemo()
+    {
+        Set<String> numbers = readFile()
+                // collect stream elements to another target. Collectors provee varios factory methods
+                // para obtener Collection comunes. Collectors.toSet() / Collectors.toList()
+                // TreeSet<String> result = stream.collect(Collectors.toCollection(TreeSet::new));
+                .collect(Collectors.toSet());
+        show("Reading file ", numbers);
+    }
+    
+    public static void iteratingElementsDemo()
+    {
+        Iterator<Integer> iter = Stream.iterate(0, n -> n + 1).limit(10)
+                .iterator();
+        while(iter.hasNext())
+        {
+            System.out.print(iter.next());
+        }
+        System.out.println("");
+    }
+    
+    public static void interatingIntegerDemo()
+    {
+        Object[] numbers = Stream.iterate(0, n -> n + 1).limit(10).toArray();
+        System.out.println("Object Array: " + numbers); // notar es un array object
+        try
+        {
+            Integer number = (Integer) numbers[0];
+            System.out.println("Number: " +  number);
+            System.out.println("La siguiente sentencia lanzara una exception: ");
+            Integer[] numbers2 = (Integer[]) numbers;
+        }
+        catch(ClassCastException ex)
+        {
+            System.out.println(ex);
+        }
+    }
+    
+    public static void integerArrayDemo()
+    {
+        Integer[] number3 = Stream.iterate(0, n -> n + 1).limit(10).toArray(Integer[]::new);
+        Integer number4 = (Integer) number3[0];
+        System.out.print("Integer array: " + number4);
+        System.out.println("");
     }
     
     public static void main(String[] args) {
-        Set<String> numbers = readFile()
-                .collect(Collectors.toSet());
-        show("Reading file", numbers);
+        printReadFileDemo();
+        iteratingElementsDemo();
+        interatingIntegerDemo();
+        integerArrayDemo();
     }
 }
