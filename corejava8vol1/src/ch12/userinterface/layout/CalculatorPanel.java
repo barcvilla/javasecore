53d49ba1d6e649bbfc5075f955ba7b66a184b451
Carlos Villanueva <ceva_19@hotmail.com>
2018-04-30 18:57:47 -0300

    Chapter 12 - User Interface

---
+++ corejava8vol1/src/ch12/userinterface/layout/CalculatorPanel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch12.userinterface.layout;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author PC
+ */
+public class CalculatorPanel extends JPanel{
+    
+    public CalculatorPanel()
+    {
+        setLayout(new BorderLayout());
+    }
+}
