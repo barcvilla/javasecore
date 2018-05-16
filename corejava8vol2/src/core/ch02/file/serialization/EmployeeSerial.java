11d4915dde26c9a9a3fe6b86811655aca34c6e63
Carlos Villanueva <ceva_19@hotmail.com>
2018-05-16 09:48:47 -0300

    Working with Files and I/O

--- corejava8vol2/src/core/ch02/file/serialization/EmployeeSerial.java
+++ corejava8vol2/src/core/ch02/file/serialization/EmployeeSerial.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package core.ch02.serialization;
+package core.ch02.file.serialization;
 
 import core.ch02.file.write.*;
 
